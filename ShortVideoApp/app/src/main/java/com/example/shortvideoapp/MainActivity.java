abc78eca55925acd65bb259713dacb4c30051269
hjy <uangiau@163.com>
2020-05-17 22:00:57 +0800

    update Json

--- ShortVideoApp/app/src/main/java/com/example/shortvideoapp/MainActivity.java
+++ ShortVideoApp/app/src/main/java/com/example/shortvideoapp/MainActivity.java
@@ -1,14 +1,94 @@
 package com.example.shortvideoapp;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<VideoInfo> videoList = new ArrayList<>();
+    private int flag=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        okHttpData();
+        while(flag==0);
+
+        Log.d("List",String.valueOf(videoList.size()));
+        for(int i=0;i<videoList.size();i++){
+            Log.d("List",videoList.get(i).getId());
+            Log.d("List",videoList.get(i).getFeedUrl());
+            Log.d("List",videoList.get(i).getNickName());
+            Log.d("List",videoList.get(i).getDescription());
+            Log.d("List",videoList.get(i).getLikeCount());
+            Log.d("List",videoList.get(i).getAvatar());
+        }
+    }
+
+    //获取json内容
+    private void okHttpData() {
+        new Thread(new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.P)
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder().url("https://beiyou.bytedance.com/api/invoke/video/invoke/video").build();
+                try {
+                    Response response = client.newCall(request).execute();
+                    String data = response.body().string();
+                    parseJsonString(data);
+                    flag=1;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+
+    //解析json内容
+    private void parseJsonString(String data) {
+        if (data != null) {
+            try {
+                JSONArray jsonArray = new JSONArray(data);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    String id = jsonArray.getJSONObject(i).getString("_id");
+                    String feedUrl = jsonArray.getJSONObject(i).getString("feedurl");
+                    String nickName = jsonArray.getJSONObject(i).getString("nickname");
+                    String description = jsonArray.getJSONObject(i).getString("description");
+                    String likeCount = jsonArray.getJSONObject(i).getString("likecount");
+                    String avatar = jsonArray.getJSONObject(i).getString("avatar");
+
+                    //放进list
+                    VideoInfo videoInfo=new VideoInfo();
+                    videoInfo.setId(id);
+                    videoInfo.setFeedUrl(feedUrl);
+                    videoInfo.setNickName(nickName);
+                    videoInfo.setDescription(description);
+                    videoInfo.setLikeCount(likeCount);
+                    videoInfo.setAvatar(avatar);
+                    videoList.add(videoInfo);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ ShortVideoApp/app/src/main/java/com/example/shortvideoapp/VideoInfo.java
@@ -0,0 +1,48 @@
+package com.example.shortvideoapp;
+
+public class VideoInfo {
+    private String id;
+    private String feedUrl;
+    private String nickName;
+    private String description;
+    private String likeCount;
+    private String avatar;
+
+    public void setId(String id){
+        this.id=id;
+    }
+    public void setFeedUrl(String feedUrl){
+        this.feedUrl=feedUrl;
+    }
+    public void setNickName(String nickName){
+        this.nickName=nickName;
+    }
+    public void setDescription(String description){
+        this.description=description;
+    }
+    public void setLikeCount(String likeCount){
+        this.likeCount=likeCount;
+    }
+    public void setAvatar(String avatar){
+        this.avatar=avatar;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+    public String getFeedUrl(){
+        return this.feedUrl;
+    }
+    public String getNickName(){
+        return this.nickName;
+    }
+    public String getDescription(){
+        return this.description;
+    }
+    public String getLikeCount(){
+        return this.likeCount;
+    }
+    public String getAvatar(){
+        return this.avatar;
+    }
+}
